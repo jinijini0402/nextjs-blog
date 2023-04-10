7cbbe8290e2a5f197e9537397a5039be95800468
jinijini0402 <jinijini0402@gmail.com>
2023-04-10 13:01:03 +0900

    commit

--- pages/posts/first-page.js
+++ pages/posts/first-page.js
@@ -2,6 +2,9 @@ export default function FirstPage() {
     return (
         <div>
             MY FIRST PAGE
+        <p1>
+            My name is Yun Jin. And my student ID is 2023027938.
+        </p1>
         </div>
     )
 }
